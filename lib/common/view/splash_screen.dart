5d08607f4341c1b5b0b43ef1a9040f29440226b9
hye-inA <hye.ina0130@gmail.com>
2024-11-19 22:14:55 +0900

    SplashScreen 구현

---
+++ lib/common/view/splash_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:zeroplace/common/const/app_colors.dart';
+import 'package:zeroplace/common/const/data.dart';
+import 'package:zeroplace/common/layout/default_layout.dart';
+import 'package:zeroplace/main.dart';
+import 'package:zeroplace/zeroplace/screens/mainPage/MainPageView3/view/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    deleteToken();
+    //checkToken();
+  }
+
+  void deleteToken() async {
+    await storage.deleteAll();
+  }
+
+  void checkToken() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    /// TODO : 토큰 유효성 검증 추가
+    if (refreshToken == null || accessToken == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => LoginScreen(),
+        ),
+        (route) => false,
+      );
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => MainPage(),
+        ),
+        (route) => false,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      backgroundColor: AppColors.PRIMARY_GREEN,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/img/zerospaceLogo.png',
+              width: MediaQuery.of(context).size.width / 2,
+            ),
+            const SizedBox(height: 16.0),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
