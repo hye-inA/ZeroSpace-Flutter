6e699029a869421a2d5b5aa4340491233c2bdb19
hye-inA <hye.ina0130@gmail.com>
2024-11-29 21:29:52 +0900

    시간대 처리 로직 구현

--- lib/studyroom/view/studyroom_select_dateTIme_screen.dart
+++ lib/studyroom/view/studyroom_select_dateTIme_screen.dart
@@ -23,8 +23,90 @@ class StudyroomSelectDatetimeScreen extends StatefulWidget {
 
 class _StudyroomSelectDatetimeScreenState
     extends State<StudyroomSelectDatetimeScreen> {
-  DateTime selectedDate = DateTime.now();
+  DateTime _selectedDate = DateTime.now();
   DateTime focusedDate = DateTime.now();
+  TimeOfDay? _startTime;
+  TimeOfDay? _endTime;
+  bool _isAfternoon = true;
+
+  final List<String> morningTimeSlots = [
+    '00:00',
+    '01:00',
+    '02:00',
+    '03:00',
+    '04:00',
+    '05:00',
+    '06:00',
+    '07:00',
+    '08:00',
+    '09:00',
+    '10:00',
+    '11:00',
+  ];
+
+  final List<String> afternoonTimeSlots = [
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00',
+  ];
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this._selectedDate = selectedDate;
+      this.focusedDate = focusedDate;
+    });
+  }
+
+  void _handleTimeSlotTap(String time) {
+    final tappedHour = int.parse(time.split(':')[0]);
+
+    // 시작 시간 선택 ( 새로운 시간 시간 선택 )
+    if (_startTime == null || _startTime != null && _endTime != null) {
+      setState(() {
+        _startTime = TimeOfDay(hour: tappedHour, minute: 0);
+        _endTime = null;
+      });
+    }
+    // 종료 시간 선택
+    else {
+      final startHour = _startTime!.hour;
+      // 시작 시간보다 이후 시간대만 선택 가능
+      if (tappedHour > startHour) {
+        setState(() {
+          _endTime = TimeOfDay(hour: tappedHour, minute: 0);
+        });
+      }
+    }
+  }
+
+  // 시간대 선택 여부 확인
+  bool isTimeSlotSelected(String time) {
+    if (_startTime == null || _endTime == null) return false;
+
+    final currentHour = int.parse(time.split(':')[0]);
+    final startHour = _startTime!.hour;
+    final endHour = _endTime!.hour;
+
+    return currentHour >= startHour && currentHour < endHour;
+  }
+
+  // 예약 가능 여부 확인
+  bool isTimeSlotReserved(String time) {
+    final hour = int.parse(time.split(':')[0]);
+    if (_isAfternoon) {
+      return hour < 24;
+    }
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,21 +192,88 @@ class _StudyroomSelectDatetimeScreenState
                                   color: Colors.grey[400])),
                         ),
                       ),
+                      const SizedBox(
+                        height: 8.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(color: Colors.grey[300]!),
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  _TimeToggleButton(
+                                    text: '오전',
+                                    isSelected: !_isAfternoon,
+                                    onTap: () =>
+                                        setState(() => _isAfternoon = false),
+                                  ),
+                                  _TimeToggleButton(
+                                    text: '오후',
+                                    isSelected: _isAfternoon,
+                                    onTap: () =>
+                                        setState(() => _isAfternoon = true),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8.0,
+                      ),
+                      Column(
+                        children: [],
+                      ),
                     ],
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
+}
 
-  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
-    setState(() {
-      this.selectedDate = selectedDate;
-      this.focusedDate = focusedDate;
-    });
+class _TimeToggleButton extends StatelessWidget {
+  final String text;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const _TimeToggleButton({
+    required this.text,
+    required this.isSelected,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+        decoration: BoxDecoration(
+          color: isSelected ? AppColors.PRIMARY_GREEN : Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: isSelected ? Colors.white : Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
